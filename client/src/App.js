7705a9b325d4d019bb3fc2a3664902a069aad761
Vlaho Ljubic <vlaho.ljubic@outlook.com>
2025-09-01 21:50:08 +0200

    new build

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Overview from "./pages/Overview";
-import TestOverview2 from "./Pages/TestOverview2";
 import AddPayment from "./pages/AddPayment";
 import "./index.css";
 
@@ -14,7 +13,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Overview />} />
           <Route path="/addPayment" element={<AddPayment />} />
-          <Route path="/pre" element={<TestOverview2 />} />
         </Routes>
       </div>
     </Router>

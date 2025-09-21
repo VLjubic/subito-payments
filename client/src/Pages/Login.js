dcbdc8927ba51429b82076e3fae1c6f2c5466c37
Vlaho Ljubic <vlaho.ljubic@outlook.com>
2025-09-21 13:15:38 +0200

    redirect after successful login

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login({ setLogedIn }) {
   const [form, setForm] = useState({
     password: "",
   });
+  const navigate = useNavigate();
 
   useEffect(() => {}, []);
 
@@ -15,6 +17,7 @@ function Login({ setLogedIn }) {
     e.preventDefault();
     if (form.password === "123123") {
       setLogedIn(true);
+      navigate("/overview");
     } else {
       alert("Wrong password");
       setForm({ password: "" });
